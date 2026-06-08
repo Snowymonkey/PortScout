7ff142fda44a780952e874d8f800a255fe017b3c
Snowymonkey <143772406+Snowymonkey@users.noreply.github.com>
2026-06-08 13:58:30 -0500

    Added Threading to port scanning and added cli arguments using argparse.

---
+++ network_utilities.py
@@ -0,0 +1,19 @@
+import socket
+
+def lookup(website):
+    try:
+        ip = socket.getaddrinfo(website, 0)[3][4][0]
+        return ip
+    except:
+        print("Error, no IP found")
+
+def connect(website, port):
+    try:
+        socket.create_connection((website, port), timeout=5.0)
+        return f"Port {port} is open"
+    except socket.gaierror:
+        return "No website found"
+    except socket.timeout:
+        return f"{port} timeout"
+    except:
+        return f"Port {port} is closed"
--- portscout.py
+++ portscout.py
@@ -1,4 +1,6 @@
 import argparse
+from concurrent.futures import ThreadPoolExecutor
+from network_utilities import lookup, connect
 
 parser = argparse.ArgumentParser(
     prog='PortScout',
@@ -8,7 +10,7 @@ parser = argparse.ArgumentParser(
 
 group = parser.add_mutually_exclusive_group(required=True)
 
-parser.add_argument("target")
+parser.add_argument("target", help="Host to add or website to lookup")
 
 group.add_argument(
     "-p",
@@ -19,14 +21,39 @@ group.add_argument(
 group.add_argument(
     "-pr",
     "--port_range",
-    help = "Sets range of ports to try connections with. Must be in format LowerPort-UpperPort."
+    help = "Sets range of ports to try connections with inclusively. Must be in format LowerPort-UpperPort."
 )
 
 group.add_argument(
     "-l",
     "--lookup",
-    help="Returns IP of given website."
+    action="store_true",
+    help="Returns the IP of given website."
 )
 
-
 args = parser.parse_args()
+
+if args.lookup:
+    print(f"IP : {lookup(args.target)}")
+
+elif args.port:
+    connect(args.target, args.port)
+
+elif args.port_range:
+    if "-" in args.port_range:
+        try:
+            lowerPort = int(args.port_range.split("-")[0])
+            upperPort = int(args.port_range.split("-")[1])
+            ports = list(range(lowerPort, upperPort+1))
+
+            with ThreadPoolExecutor(max_workers=100) as executor:
+                results = executor.map(lambda p: connect(args.target, p), ports)
+
+            for result in results:
+                print(result)
+
+        except:
+            print("Invalid port range")
+        
+    else:
+        print("Invalid port range. Must be in format LowerPort-UpperPort")
